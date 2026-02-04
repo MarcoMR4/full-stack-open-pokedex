790f3396513078e69eb18672684efd4dc1c5f4a7
Marco MR <marcomr9641@gmail.com>
2026-02-04 13:48:06 -0600

    feat: Add endpoint to return current application version

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const app = express()
 
 // get the port from env variable
 const PORT = 5000
+const CURRENT_VERSION = '1.0.0'
 
 app.use(express.static('dist'))
+
+app.get('/version', (req, res) => {
+  res.send(CURRENT_VERSION)
+})
 
 app.listen(PORT, () => {})
